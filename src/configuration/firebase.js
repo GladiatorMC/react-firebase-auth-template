901eac00f73e7d12f4dfc64700b280a886787eac
C0d3R <jejojora2@gmail.com>
2020-02-01 18:01:43 -0500

    Update firebase.js

--- src/configuration/firebase.js
+++ src/configuration/firebase.js
@@ -2,8 +2,4 @@ import Firebase from "firebase";
 
 export const firebase = Firebase.initializeApp({
     // your api keys & stuff
-<<<<<<< HEAD
 });
-=======
-});
->>>>>>> d00485f49c83932bc458ad4f71969beacfaea09e
